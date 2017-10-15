6ced3826451a7c91e3f0f027f578b5c0faddd43f
danglinsheng <danglinsheng@alibaba-inc.com>
2017-10-15 23:31:47 +0800

    add string init and ops

--- 20171015/string_t.cpp
+++ 20171015/string_t.cpp
@@ -14,6 +14,7 @@ void string_init() {
 	string s4(10, '1');
 	cout<<"s4:"<<s4<<endl;
 
+#if 0
 	string s;
 	cout<<"input string to display, and you can press 'end' to finish input!\n";
 	while(cin>>s) {
@@ -29,6 +30,7 @@ void string_init() {
 			break;
 		cout<<"line:"<<line<<endl;
 	}
+#endif
 
 	return;
 }
@@ -36,6 +38,30 @@ void string_init() {
 
 
 void string_ops() {
+	string s1("abcd1234");
+	string s2="abcd1234";
+	string s3(s1);
+	string s4(10,'a');
+	string s5;
+
+	cout<<"is s1 empty? "<<s1.empty()<<". "<<"is s5 empty? "<<s5.empty()<<". "<<endl;
+	cout<<"size of s1:"<<s1.size()<<" size of s4:"<<s4.size()<<endl;
+
+	cout<<"s2[3] is :"<<s2[3]<<endl;
+	cout<<"s3+s4:"<<s3+s4<<endl;
+	s5=s4;
+	cout<<"s5(=s4):"<<s5<<endl;
+
+	cout<<"is s1=s2?"<<(s1==s2)<<". "<<"is s1=s3?"<<(s1==s3)<<". "<<"is s4=s5?"<<(s4==s5)<<"."<<endl;
+	cout<<"is s4>s1?"<<(s4>s1)<<endl;
+
+
+
+
+
+
+
+
 
 
 
