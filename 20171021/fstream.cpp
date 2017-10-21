847bba34c1a159e10a5c08c49d4297ff72236d8f
danglinsheng <danglinsheng@alibaba-inc.com>
2017-10-21 23:35:50 +0800

    fsteam ops

--- 20171021/fstream.cpp
+++ 20171021/fstream.cpp
@@ -4,26 +4,37 @@
 using namespace std;
 
 void fstream_init() {
-	string ifile("i.txt"), ofile("o.txt");
+	string ifile("i.txt");
 	ifstream infile(ifile.c_str());
-
 	cout<<infile.good()<<infile.eof()<<infile.fail()<<infile.bad()<<endl;
-	
 	infile.open("tmp/i.txt");
 	char c = infile.get();
-
 	while(!infile.eof()) {
 		cout<<c;
 		c = infile.get();
 	}
-
 	cout<<infile.good()<<infile.eof()<<infile.fail()<<infile.bad()<<endl;
-	
+	infile.close();
+
+
+	string ofile("o.txt");	
 	ofstream outfile(ofile.c_str());
 	outfile<<"abcd1234"<<endl;
 	cout<<outfile.good()<<outfile.eof()<<outfile.fail()<<outfile.bad()<<endl;
-
-	infile.close();
+	string file("tmp/io.txt");	
+	fstream iofile(file.c_str());
 	outfile.close();
+
+
+	iofile<<"1234abcd"<<endl;
+	cout<<iofile.good()<<iofile.eof()<<iofile.fail()<<iofile.bad()<<endl;
+	iofile.seekp(ios::beg);
+	c = iofile.get();
+	while(!iofile.eof()) {
+		cout<<c;
+		c = iofile.get();
+	}
+	cout<<iofile.good()<<iofile.eof()<<iofile.fail()<<iofile.bad()<<endl;
+	iofile.close();
 
 }
