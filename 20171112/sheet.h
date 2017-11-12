e6abc18f066f4fd0471b1e8a4f0a957f21de2021
danglinsheng <danglinsheng@alibaba-inc.com>
2017-11-12 15:44:51 +0800

    test sheet

---
+++ 20171112/sheet.h
@@ -0,0 +1,64 @@
+#include<string>
+using namespace std;
+
+class cell {
+public:
+	cell();
+	cell(string s);
+	cell(double v);
+	cell(const cell &data);
+	~cell();
+
+	cell &operator=(const cell &data);
+	cell &operator+(const cell &data);
+
+	void setDoubleValue(const double v);
+	void setStringValue(const string s);
+
+	double getDoubleValue();
+	string &getStringValue();
+
+protected:
+	double mvalue;
+	string mstr;
+	double string2double(const string s);
+	string double2string(double value);
+};
+
+
+class sheet {
+public:
+  	static const int default_width = 100;
+  	static const int default_high = 100;
+
+	sheet(int w, int h);
+  	sheet(const sheet &data);
+
+	virtual ~sheet();
+
+	sheet &operator=(const sheet &data);
+	void setCell(int x, int y, const cell &cell);
+	cell &getCell(int x, int y);
+	
+protected:
+	bool inRange(int value, int upper);
+	void copyFrom(const sheet &data);
+	cell **mcell;
+	int mw,mh;
+};
+
+class mysheet {
+public:
+	mysheet(int w, int h);
+	mysheet(const mysheet &data);
+	
+	virtual ~mysheet();
+
+	mysheet &operator=(const mysheet &data);
+	void setCell(int x, int y, const cell &cell);
+	cell &getCell(int x, int y);
+
+protected:
+	sheet *msheet;
+};
+
