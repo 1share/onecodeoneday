d799b7a71c777d0d400b94bca9fb1a1151aacd0c
danglinsheng <danglinsheng@alibaba-inc.com>
2017-11-12 21:47:15 +0800

    pc

---
+++ 20171112/pc4.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+class parent {
+public:
+	parent() {cout<<1<<endl;};
+	virtual ~parent() {cout<<11<<endl;};
+};
+
+class some {
+public:
+	some() {cout<<2<<endl;};
+	~some() {cout<<22<<endl;};
+};
+
+class child:public parent {
+public:
+	child(){cout<<3<<endl;};
+	~child(){cout<<33<<endl;};
+protected:
+	some msome;
+};
+
+int main() {
+	parent *p = new child();
+	delete p;
+
+	return 0;
+}
+
