df859c3328313f9d21058c91af1a4f1473615e1b
dark <danglinsheng@126.com>
2018-04-14 23:20:47 +0800

    new ffi

--- 20180312/ffimyc.c
+++ 20180312/ffimyc.c
@@ -1,47 +1,47 @@
 #include <stdio.h>
 
 struct constr_t {
-    int a;
-    int b;
-    struct innerstr {
-        int x;
-        int y;
-    } c;
+	int a;
+	int b;
+	struct innerstr {
+		int x;
+		int y;
+	} c;
 };
 
 void print_constr_t(struct constr_t t)
 {
-    printf("a:%d\n", t.a);
-    printf("b:%d\n", t.b);
-    printf("c.x:%d\n", t.c.x);
-    printf("c.y:%d\n", t.c.y);
+	printf("a:%d\n", t.a);
+	printf("b:%d\n", t.b);
+	printf("c.x:%d\n", t.c.x);
+	printf("c.y:%d\n", t.c.y);
 }
 void print_pconstr_t(struct constr_t *t)
 {
-    printf("a:%d\n", t->a);
-    printf("b:%d\n", t->b);
-    printf("c.x:%d\n", t->c.x);
-    printf("c.y:%d\n", t->c.y);
+	printf("a:%d\n", t->a);
+	printf("b:%d\n", t->b);
+	printf("c.x:%d\n", t->c.x);
+	printf("c.y:%d\n", t->c.y);
 }
 int add(int x, int y)
 {
-    return x + y;
+	return x + y;
 }
 int addp(int *x, int *y)
 {
-    return *x+*y;
+	return *x+*y;
 }
 void print(const char *s)
 {
-    printf("%s\n", s);
+	printf("%s\n", s);
 }
 int print_i(int x)
 {
-    printf("x: %d\n", x);
+	printf("x: %d\n", x); 
+	return 0;
 }
-
 int print_pi(int *px)
 {
-    printf("px: %d\n", *px);
+	printf("px: %d\n", *px);
+	return 0;
 }
-
