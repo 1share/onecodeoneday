2879571b3e09c79cd2a1f1720e843ebf97fa1e55
dark <danglinsheng@126.com>
2018-04-21 23:29:49 +0800

    add mbuffer

--- 20180417/testbuffer.cpp
+++ 20180417/testbuffer.cpp
@@ -1,11 +1,19 @@
 #include <stdio.h>
-
+#include <string.h>
 #include "spbuffer.hpp"
 
 int main( int argc, char * argv[] )
 {
 	class SP_Buffer buf;;
+	const char *p = "abc123";
+	int len = strlen(p);
+
+	buf.append(p, len);
+
+	printf("%s\n", buf.getBuffer());
+
 
 	return 0;
 }
+
 
