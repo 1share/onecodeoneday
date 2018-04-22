72beb20a0dc773f7e4cc196d1319e3597c7e3612
danglinsheng <danglinsheng@126.com>
2018-04-22 19:32:19 +0800

    add buffer line

--- 20180422/mybuffer.cpp
+++ 20180422/mybuffer.cpp
@@ -16,22 +16,23 @@ my_bf::~my_bf() {
 }
 
 int my_bf::appendBuffer(const void *buffer, int len) {
-
-	return 0;
+	len = len <= 0 ? strlen((char *)buffer) : len;
+	
+	return evbuffer_add(mBuffer, (void *)buffer, len);
 }
 
 
 int my_bf::appendBuffer(const my_bf *mybf) {
-
-	return 0;
+	return appendBuffer(mybf->getBuffer(), mybf->getSize());
 }
 
 const void *my_bf::getBuffer() const {
-
+	((char *)(EVBUFFER_DATA(mBuffer)))[getSize()]='\0';
+	return EVBUFFER_DATA(mBuffer);
 }
 
 char *my_bf::getLine() {
-
+	return evbuffer_readline(mBuffer);
 }
 
 void my_bf::erase(int len) {
@@ -48,20 +49,40 @@ int my_bf::getSize() const {
 
 
 int my_bf::read(int fd) {
-	evbuffer_read(mBuffer, fd, -1);
+	return evbuffer_read(mBuffer, fd, -1);
 }
 
 int my_bf::write(int fd) {
-	evbuffer_write(mBuffer, fd);
+	return evbuffer_write(mBuffer, fd);
 }
 
 int my_bf::take(char *buffer, int len) {
+	len = evbuffer_remove(mBuffer, buffer, len-1);
+	buffer[len]='\0';
 
-
+	return len;
 }
 
 const void *my_bf::find(const void *key, size_t len) {
+	struct evbuffer * buffer = mBuffer;
+	u_char * what = (u_char*)key;
 
+	size_t remain = buffer->off;
+	u_char *search = buffer->buffer;
+	u_char *p;
+
+	while (remain >= len) {
+		if ((p = (u_char*)memchr(search, *what, (remain - len) + 1)) == NULL)
+			break;
+
+		if (memcmp(p, what, len) == 0)
+			return (p);
+
+		search = p + 1;
+		remain = buffer->off - (size_t)(search - buffer->buffer);
+	}
+
+	return NULL;
 }
 
 
--- 20180422/test_mybuffer.cpp
+++ 20180422/test_mybuffer.cpp
@@ -1,11 +1,47 @@
 #include<iostream>
+#include<string.h>
 
 #include "mybuffer.hpp"
 
+void printBuffer(my_bf &mybf) {
+	std::cout<<"+++++++++++++++++++++++++++++++++++"<<std::endl;
+	std::cout<<(char *)mybf.getBuffer()<<std::endl;
+	std::cout<<"--------------------------"<<std::endl;
+}
+
+void printLine(my_bf &mybf) {
+	std::cout<<"==================================="<<std::endl;
+	std::cout<<mybf.getLine()<<std::endl;
+	std::cout<<"@@@@@@@@@@@@@@@@@@@@@@@@@@"<<std::endl;
+}
 
 int main() {
-	class my_bf mybf();
+	class my_bf mybf;
 
+	mybf.appendBuffer("abc",strlen("abc"));
+	printBuffer(mybf);
+
+	mybf.appendBuffer("abcd",strlen("abcd"));
+	printBuffer(mybf);
+
+	mybf.appendBuffer("abcde",strlen("abcde"));
+	printBuffer(mybf);
+
+	mybf.appendBuffer("\n",strlen("\n"));
+	printBuffer(mybf);
+
+	mybf.appendBuffer("123\n",strlen("123\n"));
+	printBuffer(mybf);
+
+	mybf.appendBuffer("1234\n",strlen("1234\n"));
+	printBuffer(mybf);
+
+	mybf.appendBuffer("12345\n",strlen("12345\n"));
+	printBuffer(mybf);
+	printLine(mybf);
+	printLine(mybf);
+	printLine(mybf);
+	printLine(mybf);
 
 	return 0;
 }
